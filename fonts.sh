65ee44a2090f5148a824fa9d52efabe6810c337a
Egor Malyutin <abv1236@yandex.ru>
2021-11-21 15:54:36 +0600

    init

---
+++ fonts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pyftsubset --text="Egor Malyutin" fonts/header.ttf --output-file=public/header.ttf
+pyftsubset --text="Full-stack developer. Skills: JS, Go, C, Haskell, Lua" fonts/body.ttf --output-file=public/body.ttf
